aaff15b5f04477e41815e075068c499b6341bbe4
kevinhill515 <85580439+kevinhill515@users.noreply.github.com>
2026-04-10 15:33:18 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './research_hub.jsx.jsx'
+import App from './research_hub.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
